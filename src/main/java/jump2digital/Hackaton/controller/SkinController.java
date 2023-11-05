935f7621543faebeed27abbad5b1b99ff5ee8c05
Juan Milla <tokyumilla@gmail.com>
2023-11-05 19:33:31 +0100

    buy done

---
+++ src/main/java/jump2digital/Hackaton/controller/SkinController.java
@@ -0,0 +1,55 @@
+package jump2digital.Hackaton.controller;
+
+import jump2digital.Hackaton.model.entity.BoughtSkin;
+import jump2digital.Hackaton.model.entity.Skin;
+import jump2digital.Hackaton.model.service.BoughtSkinService;
+import jump2digital.Hackaton.model.service.SkinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http:localhost:9000")
+@RestController
+@RequestMapping("/skins")
+public class SkinController {
+
+    @Autowired
+    private SkinService skinService;
+
+    @Autowired
+    private BoughtSkinService boughtSkinService;
+
+    @GetMapping("/available")
+    public ResponseEntity<List<Skin>> getAllSkins() {
+        try {
+            List<Skin> skins = skinService.findAllSkins();
+            if (skins.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            else return new ResponseEntity<>(skins, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/{id}/buy")
+    public ResponseEntity<BoughtSkin> buySkin(@PathVariable("id") int id) {
+        Optional<Skin> skinData = skinService.findSkinById(id);
+
+        if(skinData.isPresent()) {
+            Skin skin = skinData.get();
+            BoughtSkin boughtSkin = new BoughtSkin(skin.getName(),skin.getType(), skin.getPrice(), skin.getColour(),skin.getHero());
+            return new ResponseEntity<>(boughtSkinService.saveBoughtSkin(boughtSkin),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+}
