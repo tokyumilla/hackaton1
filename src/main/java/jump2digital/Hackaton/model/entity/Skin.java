fc80f9f3eb77f053d8df761d1939c1ff7fd1b229
Juan Milla <tokyumilla@gmail.com>
2023-11-05 13:24:26 +0100

    probar push

---
+++ src/main/java/jump2digital/Hackaton/model/entity/Skin.java
@@ -0,0 +1,13 @@
+package jump2digital.Hackaton.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Skins")
+public class Skin {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer pk_SkinID;
+
+    @Column (name=)
+}
