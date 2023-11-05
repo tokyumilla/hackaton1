e029590efb651578168c43ca1ed91801c0d0f146
Juan Milla <tokyumilla@gmail.com>
2023-11-05 11:48:12 +0100

    inicio proyecto

---
+++ src/main/java/jump2digital/Hackaton/HackatonApplication.java
@@ -0,0 +1,13 @@
+package jump2digital.Hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jump2digital/Hackaton/HackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package jump2digital.Hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
